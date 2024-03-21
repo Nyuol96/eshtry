a3824476f1727ebda9ebf8fcccc24b8d4a1572eb
Nyuol96 <121433290+Nyuol96@users.noreply.github.com>
2024-03-21 14:27:58 +0200

    imported packeges

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:eshtry/Widgets/homescreen.dart';
+import 'package:eshtry/Widgets/loginScreen.dart';
 import 'package:eshtry/Widgets/splash_screen.dart';
 import 'package:flutter/material.dart';
 
