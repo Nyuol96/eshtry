0f0255722ab1bae8b379f8d009bc2d0f53cab11f
Nyuol96 <121433290+Nyuol96@users.noreply.github.com>
2024-03-21 13:49:04 +0200

    created splash model to navigate between 3 different screens

--- lib/Widgets/splash_screen.dart
+++ lib/Widgets/splash_screen.dart
@@ -1,49 +1,103 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:eshtry/Widgets/homescreen.dart';
-import 'package:eshtry/main.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class splashScreen extends StatefulWidget {
-  const splashScreen({super.key});
+class boardingmodel {
+  final String image;
+  final String title;
+  final String body;
 
-  @override
-  State<splashScreen> createState() => _splashScreenState();
+  boardingmodel({required this.image, required this.title, required this.body});
 }
 
-class _splashScreenState extends State<splashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _navigatetohome();
-  }
-
-  _navigatetohome() async {
-    await Future.delayed(Duration(milliseconds: 1500), () {});
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => HomeScreen()));
-  }
-
+class splashScreen extends StatelessWidget {
+  var boardContorller = PageController();
+  List<boardingmodel> boarding = [
+    boardingmodel(
+        image: 'assets/CDD5A9CD-A97C-47CF-B556-01F465535556_1_201_a.jpeg',
+        title: 'Boarding',
+        body: 'Indicator 1'),
+    boardingmodel(
+        image: 'assets/CDD5A9CD-A97C-47CF-B556-01F465535556_1_201_a.jpeg',
+        title: 'Boarding',
+        body: 'Indicator 2'),
+    boardingmodel(
+        image: 'assets/CDD5A9CD-A97C-47CF-B556-01F465535556_1_201_a.jpeg',
+        title: 'Boarding',
+        body: 'Indicator 3'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      duration: 1000,
-      splash: Column(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Expanded(
+            child: PageView.builder(
+              physics: BouncingScrollPhysics(),
+              controller: boardContorller,
+              itemBuilder: ((context, index) =>
+                  buildBoardingItem(boarding[index])),
+              itemCount: boarding.length,
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
+              SmoothPageIndicator(
+                  controller: boardContorller,
+                  effect: ExpandingDotsEffect(
+                      dotColor: Colors.grey,
+                      dotHeight: 10,
+                      dotWidth: 10,
+                      expansionFactor: 4,
+                      spacing: 5.0),
+                  count: boarding.length),
+              Spacer(),
+              FloatingActionButton(
+                onPressed: () {
+                  boardContorller.nextPage(
+                      duration: Duration(milliseconds: 750),
+                      curve: Curves.fastLinearToSlowEaseIn);
+                },
+                child: Icon(Icons.arrow_forward_ios),
+              )
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+
+  Widget buildBoardingItem(boardingmodel model) => Column(
         children: [
-          Image.asset(
-              'assets/CDD5A9CD-A97C-47CF-B556-01F465535556_1_201_a.jpeg'),
+          Expanded(
+            child: Image.asset('${model.image}'),
+          ),
+          SizedBox(
+            height: 30,
+          ),
           Text(
-            'eShtry',
+            '${model.title}',
             style: TextStyle(
                 fontSize: 26,
                 fontWeight: FontWeight.bold,
                 color: Colors.green[700]),
-          )
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            '${model.body}',
+            style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.green[700]),
+          ),
+          SizedBox(
+            height: 30,
+          ),
         ],
-      ),
-      nextScreen: HomeScreen(),
-      backgroundColor: Colors.white,
-      splashIconSize: 300,
-      splashTransition: SplashTransition.slideTransition,
-    );
-  }
+      );
 }
