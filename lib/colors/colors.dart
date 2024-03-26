76f36f0c0500cf45a3fbbdb3fa1ed146344d90f6
Nyuol96 <121433290+Nyuol96@users.noreply.github.com>
2024-03-26 11:04:05 +0200

    created default form field

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const defaultColor = Colors.blue;
+
+TextStyle defaultTitleTextStyle = TextStyle(
+    fontFamily: 'Righteous', fontSize: 15, color: Colors.grey.shade600);
--- lib/components.dart
+++ lib/components.dart
@@ -1,3 +1,4 @@
+import 'package:eshtry/colors/colors.dart';
 import 'package:flutter/material.dart';
 
 void navigateTo(context, widget) => Navigator.push(
@@ -17,3 +18,57 @@ void navigateAndFinish(
           builder: (context) => widget,
         ),
         (route) => false);
+
+Widget defaultTextButton({required onPressed, required String text}) =>
+    TextButton(
+      onPressed: onPressed,
+      child: Text(
+        text.toUpperCase(),
+        style: defaultTitleTextStyle.copyWith(
+            fontSize: 15, color: Colors.deepOrange),
+      ),
+    );
+
+Widget defaultFormField({
+  String? initalValue,
+  String? hintText,
+  TextStyle? style,
+  TextStyle? hintStyle,
+  bool? isEnabled,
+  @required TextEditingController? controller,
+  @required TextInputType? keyboardType,
+  Function(String?)? onSubmit,
+  var onChanged,
+  String? Function(String?)? validate,
+  var suffixPressedFuncion,
+  String? label,
+  IconData? prefixIcon,
+  IconData? suffixIcon,
+  bool isPassword = false,
+}) =>
+    TextFormField(
+        enabled: isEnabled,
+        onSaved: (newValue) {
+          onSubmit;
+        },
+        initialValue: initalValue,
+        style: style,
+        validator: validate,
+        obscureText: isPassword,
+        onFieldSubmitted: onSubmit,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: hintStyle ??
+              defaultTitleTextStyle.copyWith(color: Colors.black12),
+          border: InputBorder.none,
+          label: Text(
+            label ?? '',
+            style: defaultTitleTextStyle.copyWith(fontSize: 15),
+          ),
+          prefixIcon: Icon(prefixIcon),
+          suffixIcon: IconButton(
+            icon: Icon(suffixIcon),
+            onPressed: suffixPressedFuncion,
+          ),
+        ));
